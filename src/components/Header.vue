360776144eb9d808eb49214e4b0d1a4671cec382
Keila <keila.valdez@seis10.com>
2021-05-02 18:51:45 -0500

    Agrega archivos iniciales

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="Header">
+  </div>
+</template>
+<script>
+import '@/assets/css/Header.css'
+export default {
+  name: 'Header'
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      'sans': [
+        'system-ui',
+        'BlinkMacSystemFont',
+        '-apple-system',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Fira Sans',
+        'Droid Sans',
+        'Helvetica Neue',
+        'sans-serif',
+      ],
+      'serif': [
+        'Constantia',
+        'Lucida Bright',
+        'Lucidabright',
+        'Lucida Serif',
+        'Lucida',
+        'DejaVu Serif',
+        'Bitstream Vera Serif',
+        'Liberation Serif',
+        'Georgia',
+        'serif',
+      ],
+      'mono': [
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        'Liberation Mono',
+        'Courier New',
+        'monospace',
+      ]
+    },
+    colors: colors,
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
