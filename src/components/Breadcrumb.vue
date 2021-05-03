c6d0bd520d318295bdea813fb78aa4f2798c19f1
Keila <keila.valdez@seis10.com>
2021-05-02 23:08:42 -0500

    Separa componentes cardcomplete y breadcrumb

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="breadcrumb" class="bg-secondary h-12 py-2 px-4">
+    <div class="inline-block align-middle cursor-pointer hover:text-white" @click="goToHome()">Home </div><div class="inline-block align-middle mx-2">/</div><div class="inline-block align-middle font-bold" v-if="title.length > 0">{{title}}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Breadcrumb',
+  props:{
+    title:{
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,50 +1,31 @@
 <template>
   <div>
-    <div id="breadcrumb" class="bg-secondary">
-      <div class="inline-block align-middle cursor-pointer hover:text-white" @click="goToHome()">Home </div><div class="inline-block align-middle mx-2">/</div><div class="inline-block align-middle font-bold">{{movie.title}}</div>
-    </div>
-    <div class="p-4">
-      <div v-if="error" class="text-center font-bold text-3xl">Not Found</div>
-      <div v-if="!error && movie != null" class="p-6 shadow-lg rounded-lg bg-white">
-        <div class="text-3xl mb-2 text-center">{{movie.title}}</div>
-        <div class="mb-2 text-center">
-          <div class="inline-block">
-            <iconBase :width="18" :height="18" iconColor="" class="align-middle inline-block" title="Running time"><iconClock/></iconBase>  
-            <span class="align-middle inline-block ml-1">{{movie.running_time}}</span>
-          </div>
-          <div class="inline-block ml-5">
-            <iconBase :width="18" :height="18" iconColor="" class="align-middle inline-block" title="Score"><iconStar/></iconBase>  
-            <span class="align-middle inline-block ml-1">{{movie.rt_score}}</span>
-          </div>
-          <div class="inline-block ml-5">
-            <iconBase :width="18" :height="18" iconColor="" class="align-middle inline-block" title="Relese date"><iconCalendar/></iconBase>  
-            <span class="align-middle inline-block ml-1">{{movie.release_date}}</span>
-          </div>
-        </div>
-        <div class="w-full sm:w-1/2 sm:inline-block py-2 pl-4 mt-4"><span class="font-bold">Original title: </span>{{movie.original_title}}</div>
-        <div class="w-full sm:w-1/2 sm:inline-block py-2 pl-4"><span class="font-bold">Original title romanised: </span>{{movie.original_title_romanised}}</div>
-        <div class="w-full sm:w-1/2 sm:inline-block py-2 pl-4"><span class="font-bold">Director: </span>{{movie.director}}</div>
-        <div class="w-full sm:w-1/2 sm:inline-block py-2 pl-4"><span class="font-bold">Producer: </span>{{movie.producer}}</div>
-        <p class="text-justify mt-8">{{movie.description}}</p>
-      </div>
-    </div>
+    <breadcrumb :title="movie.title"/>
+    <div v-if="error" class="text-center font-bold text-3xl p-4">Not Found</div>
+    <card-complete
+      v-if="!error && movie != null"
+      :title="movie.title"
+      :producer="movie.producer"
+      :director="movie.director"
+      :description="movie.description"
+      :runningTime="movie.running_time"
+      :score="movie.rt_score"
+      :releaseDate="movie.release_date"
+      :originalTitle="movie.original_title"
+      :titleRomanised="movie.original_title_romanised"
+    />
   </div>
 </template>
 <script>
-import '@/assets/css/Detail.css'
 import axios from 'axios'
-import iconBase from '@/components/icons/iconBase.vue'
-import iconStar from '@/components/icons/iconStar.vue'
-import iconClock from '@/components/icons/iconClock.vue'
-import iconCalendar from '@/components/icons/iconCalendar.vue'
+import cardComplete from '@/components/CardComplete.vue'
+import breadcrumb from '@/components/Breadcrumb.vue'
 
 export default {
   name: 'Detail',
   components:{
-    iconBase,
-    iconStar,
-    iconClock,
-    iconCalendar
+    cardComplete,
+    breadcrumb
   },
   data () {
     return {
