1b5b9eae02b299e946a395502e35cd98a5ae9f3d
Keila <keila.valdez@seis10.com>
2021-05-02 21:11:20 -0500

    Elimina buscador del header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,11 @@
     <div id="Header" class="bg-primary py-2 px-4">
       <img src="@/assets/img/logo.png" class="h-28">
     </div>
-    <div class="h-10 bg-secondary">
-    </div>
   </div>
 </template>
 <script>
 import '@/assets/css/Header.css'
+
 export default {
   name: 'Header'
 }
