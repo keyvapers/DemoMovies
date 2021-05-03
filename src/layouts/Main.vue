e1408496df37bdc20cbe943de25f882ec0356c29
Keila <keila.valdez@seis10.com>
2021-05-02 19:31:32 -0500

    Agrega router

---
+++ src/layouts/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header/>
+    <div class="px-4 pt-2">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'Main',
+  components: {
+    Header
+  }
+}
+</script>
---
+++ src/router.js
@@ -0,0 +1,54 @@
+/*=========================================================================================
+  File Name: router.js
+  Description: Routes for vue-router. Lazy loading is enabled.
+  ----------------------------------------------------------------------------------------
+  Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
+  Author: Pixinvent
+  Author URL: http://www.themeforest.net/user/pixinvent
+==========================================================================================*/
+
+
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  scrollBehavior () {
+    return { x: 0, y: 0 }
+  },
+  routes: [
+    {
+    // =============================================================================
+    // MAIN LAYOUT ROUTES
+    // =============================================================================
+      path: '',
+      component: () => import('./layouts/Main.vue'),
+      children: [
+        // =============================================================================
+        // Theme Routes
+        // =============================================================================
+        {
+          path: '/',
+          name: 'list',
+          component: () => import('./views/List.vue')
+        },
+        {
+          path: 'detail',
+          name: 'detail',
+          component: () => import('./views/Detail.vue')
+        },
+      ]
+    },
+    
+    // Redirect to 404 page, if no match found
+    {
+      path: '*',
+      redirect: '/pages/error-404'
+    }
+  ]
+})
+
+export default router
